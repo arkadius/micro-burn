11aef0acb2d756416056f20873653f44cc89e406
Arek Burdach <arek.burdach@gmail.com>
2014-12-31 10:25:58 +0100

    Sprint combo - obsługa selekcji

--- src/main/webapp/js/SprintChart.js
+++ src/main/webapp/js/SprintChart.js
@@ -1,42 +1,41 @@
 var app = angular.module("MicroBurn", ["lift-ng"]);
-app.filter('reverse', function() {
-  return function(items) {
-    if (typeof items === 'undefined')
-      return items;
-    else
-      return items.slice().reverse();
-  };
-});
-
 app.controller("ProjectController", ['$scope', function ($scope) {
-  $scope.projectState = {};
+  $scope.projectState = {
+    sprints: []
+  };
 
-  $scope.$watch("projectState", function (newValue) {
-    console.log("projectState: ", newValue, " was changed");
+  $scope.$watch("projectState", function (projectState) {
+    $scope.selectedSprint = projectState.sprints[projectState.sprints.length-1];
   });
+
+  $scope.$watch("selectedSprint", function (sprint) {
+    $("#chart").empty();
+
+    if (typeof sprint === 'undefined')
+      return;
+
+    $.getJSON("history?sprintId=" + sprint.sprintId, function (response) {
+      var graph = new Rickshaw.Graph({
+        element: document.querySelector("#chart"),
+        renderer: "line",
+        interpolation: "step-after",
+        series: response.series
+      });
+
+      var time = new Rickshaw.Fixtures.Time();
+      var xAxes = new Rickshaw.Graph.Axis.Time({
+        graph: graph,
+        timeUnit: time.unit('day')
+      });
+
+      var yAxes = new Rickshaw.Graph.Axis.Y({
+        graph: graph
+      });
+
+      graph.render();
+    });
+  });
+
 }]);
 
 
-$.getJSON("history?sprintId=120", function (response) {
-  console.log(response.series);
-
-  var graph = new Rickshaw.Graph({
-    element: document.querySelector("#chart"),
-    renderer: "line",
-    interpolation: "step-after",
-    series: response.series
-  });
-
-  var time = new Rickshaw.Fixtures.Time();
-  var xAxes = new Rickshaw.Graph.Axis.Time({
-    graph: graph,
-    timeUnit: time.unit('day')
-  });
-
-  var yAxes = new Rickshaw.Graph.Axis.Y({
-    graph: graph
-  });
-
-  graph.render();
-
-});
